ca14efe8052221db291d8a5af948d7a4b92038fa
LMSinjorgo <94172274+LMSinjorgo@users.noreply.github.com>
2023-05-11 11:04:02 +0200

    Update Example.m
    improved some comments in the code

--- PRSM_MAXSAT_UB/Example.m
+++ PRSM_MAXSAT_UB/Example.m
@@ -8,7 +8,7 @@
 clc
 clear
 
-% Compute an bound for the included MAX-SAT instance.
+% Compute a bound for the included MAX-SAT instance.
 % More instances are available at
 % http://maxsat.ia.udl.cat/benchmarks/
 satName = 's3v70c700-1.cnf';
@@ -20,7 +20,7 @@ satDir = pwd;
 A = cnfConverter(satDir,satName);
 
 % choose monomial basis x. If left empty, the parsing algorithm will 
-% choose basis according to SOS_p^Q procedure
+% choose a basis according to SOS_p^Q procedure (see paper)
 x = [];
 
 % set the Q parameter referring to SOS_p^Q
@@ -29,7 +29,7 @@ Q = 50;
 % Parse the instance.
 PRSMinput = SOS_p_Parse(A,x,Q);
 
-% Specify lower bound, as taken from MSE-2016, 
+% Specify lower bound (LB), as taken from MSE-2016, 
 % see  http://maxsat.ia.udl.cat/detailed/complete-ms-random-table.html
 % Note: 700-21 = 679
 LB = 679; 
@@ -45,5 +45,5 @@ epsilon = 0.0001;
 % output.
 printing = true;
 
-% compute the UB. See the function 'PRSM' for more details on parameters.
+% compute the upper bound (UB). See the function 'PRSM' for more details on parameters.
 UB = PRSM(PRSMinput, LB, maxIter, maxTime, epsilon, printing);
